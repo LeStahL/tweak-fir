1342c5bb251a695af8932271f674a0824a74cf19
Alexander Kraus <nr4@z10.info>
2018-04-01 12:26:22 +0200

    added main entry point

---
+++ tweak.cpp
@@ -0,0 +1,30 @@
+/*
+ * tweak-fir - QT tool to tweak shader filter parameters for sound rendering
+ * 
+ * Copyright (C) 2018  Alexander Kraus <nr4@z10.info>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <qt5/QtWidgets/QApplication>
+
+#include "MainWindow.h"
+
+int main(int argc, char **args)
+{
+    QApplication app(argc, args);
+    MainWindow w(app);
+    w.show();
+    return app.exec();
+}
